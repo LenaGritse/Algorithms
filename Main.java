cb2887ad0a5b08171e1abf15dc40ab0a97b679e8
LenaGritse <65347294+LenaGritse@users.noreply.github.com>
2020-08-10 23:42:00 +0300

    HomeWork6

--- Main.java
+++ Main.java
@@ -1,66 +1,26 @@
-import java.io.*;
-import java.util.*;
+import java.util.Random;
 
-public class Main{
-    public static void main(String[] args){
-        //0001
-        Scanner in = new Scanner(System.in);
-        PrintWriter out = new PrintWriter(System.out);
+public class Main {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int countTree = 20;
+        int balCount = 0;
 
-        int a = in.nextInt();
-        int b = in.nextInt();
-        out.println(a + b);
-
-        out.flush();
-
-        //0025
-        Scanner in = new Scanner(System.in);
-        PrintWriter out = new PrintWriter(System.out);
-        int a = in.nextInt();
-        int b = in.nextInt();
-        if (a>b) {
-            System.out.println(">");
+        for (int i = 0; i < countTree; i++) {
+            MyTreeMap<Integer, Integer> map = new MyTreeMap<>();
+            int x = 0;
+            while (map.height() < 6) {
+                x = random.nextInt(200) - 100;
+                map.put(x, x);
+            }
+            map.delete(x);
+            if (map.isBalance()) {
+                balCount++;
+            }
         }
-        if (a<b) {
-            System.out.println("<");
-        }
-        if (a==b) {
-            System.out.println("=");
-        }
-
-        out.flush();
-
-        //0195
-        Scanner in = new Scanner(System.in);
-        PrintWriter out = new PrintWriter(System.out);
-        int n = in.nextInt();
-        int a = in.nextInt();
-        int b = in.nextInt();
-        out.println(a * b * n * 2);
-
-        out.flush();
-
-        //0773
-        Scanner in = new Scanner(System.in);
-        PrintWriter out = new PrintWriter(System.out);
-        int k = in.nextInt();
-        int m = in.nextInt();
-
-        System.out.println(k*m*k);
-
-        out.flush();
-
-        //0021
-        Scanner in = new Scanner(System.in);
-        PrintWriter out = new PrintWriter(System.out);
-        int a = in.nextInt();
-        int b = in.nextInt();
-        int c = in.nextInt();
-        int[] arr = new int[]{a,b,c};
-        Arrays.sort(arr);
-        System.out.println(arr[2] - arr[0]);
-
-        out.flush();
+        System.out.println("balCount " + balCount + " countTree " + countTree);
+        System.out.println("balanced " + (double) balCount / countTree * 100 + " %");
+        System.out.println("no balanced " + (double) (countTree - balCount) / countTree * 100 + " %");
 
     }
 }
